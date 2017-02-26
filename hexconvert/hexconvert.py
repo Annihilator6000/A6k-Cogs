26d02dae010eefb6da80f431480de5c8e923764f
Annihilator6000 <8xovermsoctober@gmail.com>
2017-02-25 19:26:46 -0500

    All fixed up

--- hexconvert/hexconvert.py
+++ hexconvert/hexconvert.py
@@ -1,6 +1,6 @@
 import discord
 import os
-import asyncio
+import binascii
 
 from discord.ext import commands
 from cogs.utils.dataIO import fileIO
@@ -8,15 +8,18 @@ from cogs.utils.chat_formatting import *
 from __main__ import send_cmd_help
 
 class Convert(object):
-    def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
-        bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
+    def text_to_bits(self, text, encoding='utf-8', errors='surrogatepass'):
+        bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
         return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-    def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
-        n = int(bits, 2)
-        return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
+    def text_from_bits(self, bits, encoding='utf-8', errors='surrogatepass'):
+        print(bits)
+        n = int(bits.replace(" ", ""), 2)
+        hex_string = '%x' % n
+        nlen = len(hex_string)
+        return binascii.unhexlify(hex_string.zfill(nlen + (nlen & 1))).decode(encoding, errors)
     
-class Hexconvert(object):
+class hexconvert(object):
     def __init__(self, bot):
         self.bot = bot
         self.convert = Convert()
@@ -27,16 +30,16 @@ class Hexconvert(object):
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
     
-    @Hexconvert.command(pass_context=True, name="toascii")
+    @hexconvert.command(pass_context=True, name="toascii")
     async def _toascii(self, ctx, hexadecimal: str):
-    """Convert an appended Hex string to ASCII. If there are spaces in the Hex string it must be encased in quotes."""
+        """Convert an appended Hex string to ASCII. If there are spaces in the Hex string it must be encased in quotes."""
         await self.bot.say(self.convert.text_from_bits(hexadecimal))
     
-    @Hexconvert.command(pass_context=True, name="tohex")
+    @hexconvert.command(pass_context=True, name="tohex")
     async def _tohex(self, ctx, ascii: str):
-    """Convert an appended ASCII string to Hex. If there are spaces in the ASCII string it must be encased in quotes."""
+        """Convert an appended ASCII string to Hex. If there are spaces in the ASCII string it must be encased in quotes."""
         await self.bot.say(self.convert.text_to_bits(ascii))
     
 def setup(bot):
-    n = Hexconvert(bot)
+    n = hexconvert(bot)
     bot.add_cog(n)
